919d08a38c3863ea24f481a49869070e386aead5
lsolesen <lsolesen@users.noreply.github.com>
2021-05-17 07:30:46 +0000

    Github Actions - Mon May 17 07:30:46 UTC 2021

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Populære blog posts i 2020 på Motionsplan.dk",
+        "excerpt":"Motionsplan er et af de sites, jeg er redaktør for. Hvert år kigger jeg lidt tilbage på de populære indlæg. Her samler jeg op på de mest populære blog posts på Motionsplan i løbet af 2020. Der er nogle ældre blog posts, men en del af de nye artikler har...","categories": ["Webclips"],
+        "tags": ["webclips"],
+        "url": "/webclips/motionsplan/",
+        "teaser": "https://images.unsplash.com/photo-1538503529202-7a0e79cbb6f6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80"
+      }]
